0e890c7fa547c3e4e1dd010a7ee0da4e3f01ab2f
Pigeon <160717703+lePigeon007@users.noreply.github.com>
2024-07-15 17:41:47 +0930

    Update auto_update.sh

--- auto_update.sh
+++ auto_update.sh
@@ -9,23 +9,24 @@ if [[ ${UID} -ne 0 ]]
 then
 	echo "You did not execute as Sudo."
 	echo "Please run this script as Sudo!"
-	echo "Attempted non-sudo execute by $(whoami) at $(date): ABORTED" >> /home/gyp/Documents/logs/update_logger.txt
-	exit 1
+	echo "Attempted non-sudo execute by $(whoami) at $(date): ABORTED" >> /home/dir/Documents/logs/update_logger.txt
+	echo " " >> /home/dir/Documents/logs/update_logger.txt
+ 	exit 1
 else
-	echo "Executed with Sudo privileges, Continuing:" >> /home/gyp/Documents/logs/update_logger.txt
+	echo "Executed with Sudo privileges, Continuing:" >> /home/dir/Documents/logs/update_logger.txt
 fi
 
 # Perform update and upgrade:
 sudo apt-get update && sudo apt-get upgrade -y
-echo "		-> Update and Upgrade executed successfully." >> /home/gyp/Documents/logs/update_logger.txt
+echo "		-> Update and Upgrade executed successfully." >> /home/dir/Documents/logs/update_logger.txt
 
 # Perform cleanup:
 apt autoremove
-echo "		-> Cleanup executed successfully." >> /home/gyp/Documents/logs/update_logger.txt
+echo "		-> Cleanup executed successfully." >> /home/dir/Documents/logs/update_logger.txt
 
 # Log status to the log file:
-echo "Successful update performed at $(date)" >> /home/gyp/Documents/logs/update_logger.txt
-echo " "
+echo "Successful update performed at $(date)" >> /home/dir/Documents/logs/update_logger.txt
+echo " " >> /home/dir/Documents/logs/update_logger.txt
 
 # Exit:
 exit 0
